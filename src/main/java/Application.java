31fcb2436f8949c25342fa476c64a31058ac5161
Dmitry286 <DShishlov@gmail.com>
2022-11-16 13:14:56 +0300

    - console interface: create wallet, add currency, deposit

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,19 +1,134 @@
 import models.Currency;
 import models.Wallet;
 
+import java.util.InputMismatchException;
 import java.util.List;
+import java.util.Objects;
+import java.util.Scanner;
 import java.util.stream.Collectors;
 
 public class Application {
     Wallet wallet;
+    Scanner scanner;
 
-    public Application() {
+    public Application(Scanner scanner) {
+        this.scanner = scanner;
         System.out.println("Welcome to Multi-currency Wallet");
         System.out.println("If you have a wallet already enter your username. " + "\n" +
                 "Else enter new username to create a new wallet");
+        String userName = scanner.nextLine();
+        this.createNewWallet(userName);
+        this.chooseUserStep();
     }
 
 
+
+
+    private void chooseUserStep() {
+        System.out.println("Choose your action, enter the appropriate number: ");
+        System.out.println("If you want to add new currency, enter 1");
+        System.out.println("If you want to add new deposit, enter 2");
+        System.out.println("If you want to withdraw some amount, enter 3");
+        System.out.println("If you want to set rate for two currencies, enter 4");
+        System.out.println("If you want to convert any amount of any currency to other currency, enter 5");
+        System.out.println("To look the whole balance of all currencies, enter 6");
+        System.out.println("To look total amount in any currency, enter 7");
+
+        int choiceNumber = scan();
+
+        switch (choiceNumber) {
+            case (1):
+                System.out.println("Enter currency name");
+                scanner.nextLine();
+                String currencyName = scanner.nextLine();
+                this.addNewCurrency(wallet, currencyName);
+                break;
+
+            case (2):
+                boolean flag = false;
+                //todo вынести в отдельный метод
+                while (!flag) {
+                System.out.println("Enter currency name. If you will not enter the name, " + "\n" +
+                        "the amount will be added to the first currency in your wallet");
+                scanner.nextLine();
+                String depositCurrencyName = scanner.nextLine();
+                if (depositCurrencyName != "") {
+                    if (wallet.getCurrenciesAmountMap().keySet()
+                            .stream()
+                            .map(e -> e.getName())
+                            .toList()
+                            .contains(depositCurrencyName)) {
+                        flag = true;
+                    } else {
+                        System.out.println("There is no such currency in the wallet");
+                        continue;
+                    }
+                }
+
+                System.out.println("Enter the amount");
+                double depositAmount = scanner.nextDouble();
+                if (!Objects.equals(depositCurrencyName, "")) {
+                    Currency currencyByName = wallet.findCurrencyByName(depositCurrencyName);
+                    wallet.deposit(depositAmount, currencyByName);
+                } else {
+                    wallet.deposit(depositAmount);
+                }
+                flag = true;
+                }
+                break;
+
+            case (3):
+
+                break;
+
+            case (4):
+
+                break;
+
+            case (5):
+
+                break;
+
+            case (6):
+
+                break;
+
+            case (7):
+
+                break;
+
+            default:
+
+                break;
+        }
+
+        System.out.println("If you want to quit, enter 0");
+        System.out.println("If you want to go to main menu, enter any other number");
+
+
+        int lastChoiceNumber = scanner.nextInt();
+
+        if (lastChoiceNumber == 0) {
+            Main.terminate();
+        } else {
+            this.chooseUserStep();
+        }
+
+    }
+
+    public int scan() {
+        int result;
+        while (true) {
+            try {
+                result = scanner.nextInt();
+                break;
+            } catch (InputMismatchException exception) {
+                System.out.println("You have to enter a number, please retry:");
+                scanner.nextLine();
+            }
+        }
+        return result;
+    }
 
 
     public Currency addNewCurrency(Wallet wallet, String name) {
@@ -34,7 +149,7 @@ public class Application {
             if (userList.contains(userName)) {
                 wallet = Wallet.getWalletList()
                         .stream()
-                        .filter(e -> e.getUserName() == userName)
+                        .filter(e -> Objects.equals(e.getUserName(), userName))
                         .findFirst()
                         .orElseThrow(() -> new RuntimeException("Wallet with such username does not exist"));
             } else {
@@ -42,10 +157,10 @@ public class Application {
                 System.out.println("User " + userName + " has created a new wallet");
             }
         } else {
-                wallet = new Wallet(userName);
-                System.out.println("User " + userName + " has created a new wallet");
-            }
+            wallet = new Wallet(userName);
+            System.out.println("User " + userName + " has created a new wallet");
         }
+    }
 
     public void setRate(Wallet wallet, String firstCurrency, Currency secondCurrency, double rate) {
         Currency currencyByName = wallet.findCurrencyByName(firstCurrency);
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -38,51 +38,26 @@ import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Main {
-    private static Application application;
+    protected static Application application;
     private static Scanner scanner;
-    static Wallet dimasWallet;
-    static Wallet ilyasWallet;
+
     public static void main(String[] args) {
         init();
         run();
     }
 
     public static void init() {
-        application = new Application();
         scanner = new Scanner(System.in);
-
+        application = new Application(scanner);
     }
 
     public static void run() {
-        application.createNewWallet("Dima");
-        application.createNewWallet("Ilya");
-        dimasWallet = application.wallet;
-        Currency dollar = application.addNewCurrency(dimasWallet, "dollar");
-        Currency ruble = application.addNewCurrency(dimasWallet, "ruble");
-        dimasWallet.deposit(1);
-        System.out.println(dimasWallet);
-        dimasWallet.deposit(100, ruble);
-        System.out.println(dimasWallet);
-
-        System.out.println("==================");
-        application.setRate(dimasWallet, "dollar", ruble, 60);
-        System.out.println(dollar.getRates());
-        System.out.println(ruble.getRates());
-        System.out.println("==================");
-        dimasWallet.convert(ruble, dollar, 80);
-        System.out.println(dimasWallet);
-        dimasWallet.withdraw(2.1, dollar);
-        System.out.println(dimasWallet);
-        dimasWallet.showBalance();
-        System.out.println("=========================");
-        dimasWallet.showTotal();
-        System.out.println("=========================");
-        dimasWallet.showTotal(ruble);
-        System.out.println("=========================");
 
     }
 
     public static void terminate() {
         application = null;
+        init();
+        run();
     }
 }
